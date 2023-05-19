c2273f550c14edae1f33015952ac3ae127cdd7de
玄杰 <2071985621@qq..com>
2023-05-19 15:24:06 +0800

    添加路由组件

--- src/components/RouteDemo/index.jsx
+++ src/components/RouteDemo/index.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import About from './About'
 import Home from './Home'
+// import {Link,Route} from 'react-router-dom'
 
 export default class RouteDemo extends Component {
   render() {
     return (
-      <div>
-        <About/>
-        <Home/>
-      </div>
+    //   <div>
+    //     <div>
+    //          <Link to="./About">about</Link>
+    //          <Link to="./Home">home</Link>
+    //     </div>
+    //     <div>
+    //          <Route path='./About' component={About}></Route>
+    //          <Route path='./Home' component={Home}></Route>
+    //     </div>
+    //   </div>
+   
+    <div>
+ <About/>
+    <Home/>
+    </div>
     )
   }
 }
