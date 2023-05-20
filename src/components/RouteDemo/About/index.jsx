680d04f77b2ce94732779a1845ca05028d464599
玄杰 <2071985621@qq..com>
2023-05-20 16:28:43 +0800

    添加 ant-Design

--- src/components/RouteDemo/About/index.jsx
+++ src/components/RouteDemo/About/index.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+//使用的是按需引入
+import {Button} from 'antd'
 
 export default class About extends Component {
   render() {
     return (
-      <div>我是About内容</div>
+        <div>
+            <span>这是About按键</span>
+            <Button type='primary'>primary Button</Button>
+            <Button>Default Button</Button>
+        </div>
     )
   }
 }
